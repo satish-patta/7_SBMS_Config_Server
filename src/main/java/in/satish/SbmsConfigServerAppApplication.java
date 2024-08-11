621378c47d644f65a2043b59223da8863e395db5
ihavepositiveattitude <kartikoleti@123>
2024-08-11 14:39:03 +0530

    11/08/2024 2:38

---
+++ src/main/java/in/satish/SbmsConfigServerAppApplication.java
@@ -0,0 +1,15 @@
+package in.satish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootApplication
+@EnableConfigServer
+public class SbmsConfigServerAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbmsConfigServerAppApplication.class, args);
+	}
+
+}
